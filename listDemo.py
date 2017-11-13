01294ba55621ca980654d49afd071bb5a5632022
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-13 14:25:15 -0500

    Updated from Brython Server: 11/13/2017 2:25:22 PM

--- listDemo.py
+++ listDemo.py
@@ -2,5 +2,5 @@
 #2017-11-13
 #listDemo.py - print out first and last words in a list
 
-words = input('Enter some words: ').split(' ')
+words = input('Enter some words: ').split(' ') #everytime sees (space), it adds it to list
 print(words)
