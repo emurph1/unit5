d779f2a3862795e92bbe45afb40fd0b028afabb3
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-15 13:40:32 -0500

    Updated from Brython Server: 11/15/2017 1:40:37 PM

--- longestWord.py
+++ longestWord.py
@@ -2,6 +2,5 @@
 #2017-11-15
 #longestWord.py - find longest word in set of words
 
-words = input('Enter words').split('')
-
-print(len(words))
+words = input('Enter words: ').split(' ')
+print(max(words))
