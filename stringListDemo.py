4069caf184827d0d57a041e37811fc29d3ba0903
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-17 11:32:53 -0500

    Update stringListDemo.py

--- stringListDemo.py
+++ stringListDemo.py
@@ -2,6 +2,6 @@
 #11/16/17
 #stringListDemo.py	- prints	out	words	that	start	with	a	vowel
 words = input('Enter some words: ').split('	')
-for	word in	words:
-	if	word[0]	in	'aeiouAEIOU':
+for word in words:
+	if word[0] in 'aeiouAEIOU':
 		print(word)
