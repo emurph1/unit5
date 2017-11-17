ac8c128da24f18afdc17a390c4b08f21d7e18fed
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-17 11:28:38 -0500

    Update warmUp13.py

--- warmUp13.py
+++ warmUp13.py
@@ -1,1 +1,10 @@
-
+#Emily Murphy
+#11/16/17
+#warmup13.py	- make	a	list	of	20	random	numbers,	print	out	sum,	min,	and	max
+from	random	import	randint
+numbers	=	[]
+for	i	in	range(20):
+				numbers.append(randint(1,100))
+print('The	min	is',	min(numbers))
+print('The	max	is', max(numbers))
+print('The	sum	is',	sum(numbers))
