4cdca48866ca488213e45732b66ab9c0605059c8
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-17 14:24:29 -0500

    Updated from Brython Server: 11/17/2017 2:24:34 PM

--- matrixDemo.py
+++ matrixDemo.py
@@ -2,4 +2,4 @@
 #2017-11-17
 #matrixDemo.py - how to create and use matrix (list inside of list)
 
-board = [['a','b','c']['d','e','f']['g','h','i']]
+board = [['a','b','c'],['d','e','f'],['g','h','i']]
