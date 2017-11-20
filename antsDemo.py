bb404b359a9f4634889b38e3f98091b397c9d040
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-20 14:29:54 -0500

    Updated from Brython Server: 11/20/2017 2:29:58 PM

--- antsDemo.py
+++ antsDemo.py
@@ -7,6 +7,11 @@ from ggame import *
 WIDTH = 800
 HEIGHT = 400
 
+def step():
+    for ant in data['antList']:
+        ant.x += 1
+        ant.y += 1
+
 if __name__ == '__main__':
     
     red = Color(0xFF0000,1)
@@ -18,4 +23,4 @@ if __name__ == '__main__':
     for i in range(10):
         data['antList'].append(Sprite(ant,(randint(1,WIDTH), randint(1,HEIGHT))))
     
-    App().run()
+    App().run(step)
