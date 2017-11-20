fc2bfbd81e75483361b4a3f4bb1b8d84f0715aac
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-20 14:46:51 -0500

    Updated from Brython Server: 11/20/2017 2:46:54 PM

--- displayDate.py
+++ displayDate.py
@@ -1,1 +1,10 @@
+#Emily Murphy
+#2017-11-20
+#displayDate.py - displays date
 
+from datetime import date
+from calendar import weekday
+
+today = date.today().day, date.today().month, date.today().year
+todayNum = today.weekday
+print(today)
