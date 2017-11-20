613d2090d5a63fc9c855d339d12f0b4ca2043363
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-20 14:23:17 -0500

    Updated from Brython Server: 11/20/2017 2:23:20 PM

--- antsDemo.py
+++ antsDemo.py
@@ -1,10 +1,11 @@
 #Emily Murphy
 #2017-11-20
 #antsDemo.py - how to use lists with graphics
-
+from random import randint
 from ggame import *
 
 if __name__ == '__main__':
+    
     red = Color(0xFF0000,1)
     ant = CircleAsset(10,LineStyle(1,red),red)
     
