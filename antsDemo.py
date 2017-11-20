a205de15f2a0322ea3d90d18eaf227078e023099
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-20 14:33:04 -0500

    Updated from Brython Server: 11/20/2017 2:33:08 PM

--- antsDemo.py
+++ antsDemo.py
@@ -11,8 +11,10 @@ def step():
     for ant in data['antList']:
         dx = randint(-4,3)
         dy = randint(-4,3)
-        ant.x += dx
-        ant.y += dy
+        if ant.x + dx > 0 and ant.x + dx < WIDTH:
+            ant.x += dx
+        if ant.y + dy > 0 and ant.y + dy < HEIGHT:
+            ant.y += dy
 
 if __name__ == '__main__':
     
