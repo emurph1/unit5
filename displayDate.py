fee29f0e516eba098141754dfd66ef29fb513646
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-20 14:50:30 -0500

    Updated from Brython Server: 11/20/2017 2:50:33 PM

--- displayDate.py
+++ displayDate.py
@@ -6,6 +6,6 @@ from datetime import date
 from calendar import weekday
 
 months = ['January','February','March', 'April', 'May','June','July','August','September','October','November','December']
-days - ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
 print('Today is', days[date.today().day],',', months[date.today().month],',',date.today().day,',',date.today().year)
