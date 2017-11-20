e6a3938680e06394b6ec0361c089d2648636d97b
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-20 14:22:52 -0500

    Updated from Brython Server: 11/20/2017 2:22:55 PM

--- antsDemo.py
+++ antsDemo.py
@@ -1,1 +1,13 @@
+#Emily Murphy
+#2017-11-20
+#antsDemo.py - how to use lists with graphics
 
+from ggame import *
+
+if __name__ = '__main__':
+    red = Color(0xFF0000,1)
+    ant = CircleAsset(10,LineStyle(1,red),red)
+    
+    Sprite(ant,(50,50))
+    
+    App().run()
