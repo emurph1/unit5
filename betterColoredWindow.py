f78376f5300dc04ef9cf52777d2aa7cc01778f71
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-20 14:39:15 -0500

    Updated from Brython Server: 11/20/2017 2:39:19 PM

--- betterColoredWindow.py
+++ betterColoredWindow.py
@@ -4,24 +4,12 @@
 from random import randint
 from ggame import *
 
-red = Color(0xFF0000,1)
-green = Color(0x00FF00,1)
-blue = Color(0x0000FF,1)
-black = Color(0x000000,1)
-white = Color(0xFFFFFF,1)
 
-whiteOutine = LineStyle(1, white)
-
-blueRectangle = RectangleAsset(1500, 900, whiteOutine, blue)
-greenRectangle = RectangleAsset(1500, 900, whiteOutine, green)
-blackRectangle = RectangleAsset(1500, 900, whiteOutine, black)
-redRectangle = RectangleAsset(1500, 900, whiteOutine, red)
-
-windows = [blueRectangle,greenRectangle,blackRectangle,redRectangle]
+colors = [Color(0xFF0000,1),Color(0x00FF00,1),Color(0x0000FF,1),Color(0x000000,1)]
 
 def mouseClick(Event):
     num = randint(1,4)
-    print(windows[num])
+    print(colors[num])
 
 App().listenMouseEvent('click',mouseClick)
 App().run()
