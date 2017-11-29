dfbf871b874a52be16248686353d60523e86cccb
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-29 13:46:31 -0500

    Updated from Brython Server: 11/29/2017 1:46:51 PM

--- quickSort.py
+++ quickSort.py
@@ -1,3 +1,32 @@
 #Emily Murphy
 #2017-11-27
-#.py -
+#quickSort.py -
+
+"""algorithm quicksort(A, lo, hi) is
+    if lo < hi then
+        p := partition(A, lo, hi)
+        quicksort(A, lo, p - 1 )
+        quicksort(A, p + 1, hi)
+
+algorithm partition(A, lo, hi) is
+    pivot := A[hi]
+    i := lo - 1    
+    for j := lo to hi - 1 do
+        if A[j] < pivot then
+            i := i + 1
+            swap A[i] with A[j]
+    if A[hi] < A[i + 1] then
+        swap A[i + 1] with"""
+        
+def quickSort(A, lo, hi):
+    if lo <hi:
+        p = 
+        
+def partition(A, lo, hi):
+    pivot = A[hi]
+    i = lo - 1
+    for j = lo to hi - 1:
+        if A[j] < pivot:
+            A[j], pivot = pivot, A[j]
+    if A[hi] < A[i+1]:
+        A[hi], A[i+1] = '', A[hi]
