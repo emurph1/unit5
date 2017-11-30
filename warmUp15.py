e45fd670e45679ca0ea64a1bf125421a905143ca
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-30 14:16:32 -0500

    Updated from Brython Server: 11/30/2017 2:16:46 PM

--- warmUp15.py
+++ warmUp15.py
@@ -1,1 +1,11 @@
+#Emily Murphy
+#2017-11-30
+#warmUp15.py - function that take a list of numbers as argument and returns the list where each number is doubled
 
+def doubled(n):
+    new = []
+    for i in n:
+       new.append(i*2)
+    return new
+    
+print(doubled([2,4,6]))
