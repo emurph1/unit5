abc99ca0d4824fdf0cc38eed41202760018190cf
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-30 14:22:21 -0500

    Updated from Brython Server: 11/30/2017 2:22:34 PM

--- quickSort.py
+++ quickSort.py
@@ -2,6 +2,24 @@
 #2017-11-27
 #quickSort.py -quick sort algorithm
 
+
+'''algorithm quicksort(A, lo, hi) is
+    if lo < hi then
+        p := partition(A, lo, hi)
+        quicksort(A, lo, p - 1 )
+        quicksort(A, p + 1, hi)
+
+algorithm partition(A, lo, hi) is
+    pivot := A[hi]
+    i := lo - 1    
+    for j := lo to hi - 1 do
+        if A[j] < pivot then
+            i := i + 1
+            swap A[i] with A[j]
+    if A[hi] < A[i + 1] then
+        swap A[i + 1] with A[hi]
+    return i + 1'''
+    
 from random import randint
 from time import time
 
