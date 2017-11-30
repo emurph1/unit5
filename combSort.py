a1ad4404e05634380ec98ee51facc6a6ff669c0d
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-30 13:53:26 -0500

    Update combSort.py

--- combSort.py
+++ combSort.py
@@ -33,13 +33,18 @@
          
      end loop
  end function """
- 
- def mySort(A):
+
+from random import randint
+from time import time
+
+N = 100 #how many numbers will be sorted
+
+def mySort(A):
      gap = N
      shrink = 1.3
      sort = False
-     while sort = False:
-         gap = floor(gap/shrink)
+    while sort == False:
+        gap = floor(gap/shrink)
         if gap > 1:
             sort = False
         else:
@@ -52,3 +57,26 @@
                 [i], [i+gap] = [i+gap], [i]
                 sort = False
             i += 1
+    return A
+
+if __name__ == '__main__':
+    
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+        
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = mySort(numbers)
+    t2 = time()
+       
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
+    
