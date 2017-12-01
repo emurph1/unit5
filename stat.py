f94b8966efad6b8d6814b786a57075d51c2f4084
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-01 14:50:44 -0500

    Updated from Brython Server: 12/1/2017 2:50:54 PM

--- stat.py
+++ stat.py
@@ -1,1 +1,20 @@
+#Emily Murphy
+#2017-12-01
+#stat.py - 
 
+numbers = []
+
+while True:
+    nums = input('Enter q to quit. Enter numbers: ')
+    if nums == 'q':
+        break
+    numbers.append(nums)
+numbers.sort()
+print('Max is ', numbers[len(numbers)-1])
+print('Min is ', numbers[0])
+
+total = 0
+for i in numbers:
+    total += float(i)
+    mean = total/len(numbers)
+print('Mean is ', mean)
