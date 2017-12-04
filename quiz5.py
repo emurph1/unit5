be96e840087f5a2179b21cf3fd4e8016c484521a
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-04 14:40:38 -0500

    Updated from Brython Server: 12/4/2017 2:40:39 PM

--- quiz5.py
+++ quiz5.py
@@ -3,14 +3,23 @@
 #quiz5.py
 
 from random import randint
-
-def rand5(num):
-    nums = []
-    for i in range(num):
-        nums.append(i)
-        if len(nums) == 5:
-            break
+def rand5():
+    nums= []
+    for i in range (1,6):
+        nums.append(randint(1,100))
     return nums
 
-print(rand5(randint(1,100)))
+def lastElement(element):
+    elements = []
+    elements.append(element)
+    return element[-1]
 
+def wordLengths(words):
+    word = []
+    for w in words:
+        word.append(len(w))
+    return word
+
+print(lastElement(['cat','dog','rat']))
+print(rand5())
+print(wordLengths(['the', 'cat', 'is', 'hungry']))
